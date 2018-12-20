afa84e7fc7ce09c9e7950a95aedde5909f763faa
Andrew <andrew.toghill@alces-software.com>
2018-12-20 12:55:42 +0000

    Remove success flash
    This was only meant to be there temporarily to test things briefly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,6 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if User.authenticate(params[:session][:username], params[:session][:password])
       sign_in(user)
-      flash.now[:success] = 'Logged in'
       redirect_to cluster_index_path
     else
       flash.now[:danger] = 'Invalid username or password'
