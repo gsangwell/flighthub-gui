d593c344bb8e2d9daf93ed6b49cf86ddf0afa8c0
Andrew <andrew.toghill@alces-software.com>
2019-01-07 13:12:39 +0000

    Populate the placeholder instead

--- app/assets/javascripts/userManagement.js
+++ app/assets/javascripts/userManagement.js
@@ -4,8 +4,8 @@ function populateModifyModal() {
     var email = e.relatedTarget.dataset.email;
     var id = e.relatedTarget.dataset.id;
 
-    $(e.currentTarget).find('input[name="user_modify[username]"]').val(username)
-    $(e.currentTarget).find('input[name="user_modify[email]"]').val(email)
+    $(e.currentTarget).find('input[name="user_modify[username]"]').attr('placeholder', username)
+    $(e.currentTarget).find('input[name="user_modify[email]"]').attr('placeholder', email)
     $(e.currentTarget).find('input[name="user_modify[id]"]').val(id)
   });
 }
