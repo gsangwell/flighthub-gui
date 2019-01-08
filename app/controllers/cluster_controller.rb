fff36b4548637669fa4709023fa7adc8eb7f5e1a
Andrew <andrew.toghill@alces-software.com>
2019-01-08 14:20:35 +0000

    Support rendering Markdown on the info page

--- app/controllers/cluster_controller.rb
+++ app/controllers/cluster_controller.rb
@@ -1,4 +1,8 @@
 class ClusterController < ApplicationController
+  require 'commonmarker'
+
   def index
+    file_data = IO.binread("/path/to/file")
+    @content = CommonMarker.render_html(file_data, :DEFAULT, [:table])
   end
 end
