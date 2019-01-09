186f774effc07ae22bed12bcf4b9c579d2ce95a3
Andrew <andrew.toghill@alces-software.com>
2019-01-09 11:59:41 +0000

    Implement functionality for deleting SSH keys
    A temporary file is created and the lines we want to keep are dumped
    into this file. Once this process is finished and the key we want to
    delete has not been copied to the temp file the file is closed. At which
    point the file is moved to overwrite the original file.
    
    Also included is a method for caching the file data which also handles
    separating each key.

--- app/controllers/keys_controller.rb
+++ app/controllers/keys_controller.rb
@@ -1,6 +1,23 @@
 class KeysController < ApplicationController
+  require 'fileutils'
+  require 'tempfile'
+
   def index
-    file_data = IO.binread("/path/to/ssh/keys")
-    @ssh_keys = file_data.lines.map(&:chomp)
+    @ssh_keys = file_data
+  end
+
+  def delete
+    tmp = Tempfile.new("temp_keys")
+
+    file_data.each { |l| tmp << l unless l == params[:key] }
+    tmp.close
+
+    FileUtils.mv(tmp.path, 'tmp/keys')
+  end
+
+  private
+
+  def file_data
+    data ||= IO.binread("tmp/keys").lines.map(&:chomp)
   end
 end
