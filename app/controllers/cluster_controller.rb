3f7263ce9be09c9be58841ae87eddc9c408cc947
Andrew <andrew.toghill@alces-software.com>
2019-01-10 13:42:48 +0000

    Use the config value to grab the file

--- app/controllers/cluster_controller.rb
+++ app/controllers/cluster_controller.rb
@@ -2,7 +2,7 @@ class ClusterController < ApplicationController
   require 'commonmarker'
 
   def index
-    file_data = IO.binread("/path/to/file")
+    file_data = IO.binread(Rails.application.config.appliance_information)
     @content = CommonMarker.render_html(file_data, :DEFAULT, [:table])
   end
 end
