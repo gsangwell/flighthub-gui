1ec167381f213f6f78f7fd3ad7977207b9f494a0
Andrew <andrew.toghill@alces-software.com>
2019-02-19 16:13:18 +0000

    Implement method for starting VPN

--- app/controllers/vpn_controller.rb
+++ app/controllers/vpn_controller.rb
@@ -1,4 +1,12 @@
 class VpnController < ApplicationController
   def index
   end
+
+  def start
+    if system("systemctl start openvpn@flightcenter")
+      flash[:success] = 'VPN started'
+    else
+      flash[:danger] = 'Enountered an error whilst trying to start the VPN'
+    end
+  end
 end
