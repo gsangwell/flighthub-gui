0a038695fc1b5ae28dfb171fc88d7eeebf45b0bd
Andrew <andrew.toghill@alces-software.com>
2019-02-20 12:57:28 +0000

    Move logic to separate method

--- app/controllers/cluster_controller.rb
+++ app/controllers/cluster_controller.rb
@@ -2,13 +2,20 @@ class ClusterController < ApplicationController
   require 'commonmarker'
 
   def index
-    file_data = IO.binread(Rails.application.config.appliance_information)
-    @content = CommonMarker.render_html(file_data, :DEFAULT, [:table])
-
+    @content = appliance_information
     @active = vpn_status
   end
 
   private
+
+  def appliance_information
+    file = Rails.application.config.appliance_information
+    file_data = IO.binread(file) if File.exist? file
+
+    if file_data
+      CommonMarker.render_html(file_data, :DEFAULT, [:table])
+    end
+  end
 
   def vpn_status
     run_shell_command("systemctl is-active --quiet openvpn@flightcenter")
