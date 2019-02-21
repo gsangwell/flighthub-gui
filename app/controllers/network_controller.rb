09d330f71d25a5d1f14de3a9c0de68f459c88dd5
Andrew <andrew.toghill@alces-software.com>
2019-02-21 15:14:46 +0000

    Save changes to network configuration file

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -1,4 +1,43 @@
 class NetworkController < ApplicationController
   def index
+    @lines = file_data
+  end
+
+  def edit
+    tmp = Tempfile.new("temp_vars")
+
+    file_data.each do |line|
+      if line.start_with?('export')
+        content = line.split("export")[1].split("=")
+        variable = content[0]
+        tail = content[1].split('"')[2]
+
+        line = "export#{variable}=\"#{network_params[variable]}\"#{tail}"
+      end
+
+      tmp << line
+    end
+
+    tmp.close
+
+    if `cp --no-preserve=mode,ownership #{tmp.path} #{Rails.application.config.network_variables}`
+      flash[:success] = 'Network configuration successfully modified'
+    else
+      flash[:danger] = 'Encountered an error whilst trying to modify the network configuration'
+    end
+
+    tmp.delete
+
+    redirect_to network_path
+  end
+
+  private
+
+  def network_params
+    params[:network]
+  end
+
+  def file_data
+    IO.binread(Rails.application.config.network_variables).lines.map
   end
 end
