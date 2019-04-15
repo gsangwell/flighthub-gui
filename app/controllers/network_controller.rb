f09681ba644d481f0b67237539168c60528a0ea7
Andrew <andrew.toghill@alces-software.com>
2019-04-15 16:12:34 +0100

    Refactor to work with the global scripts

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -5,7 +5,7 @@ class NetworkController < ApplicationController
            to: 'Rails.application.config'
 
   def index
-    file_lines = file_data
+    file_lines = network_get_output
     @internal_vars = file_lines.select { |l| l.include? "INTERNAL" }
     @external_vars = file_lines.select { |l| l.include? "EXTERNAL" }
   end
@@ -15,20 +15,24 @@ class NetworkController < ApplicationController
 
     file_data.each do |line|
       if line.include?("INTERNAL") || line.include?("EXTERNAL")
-        content = line.split("export")[1].split("=")
-        variable = content[0]
-        tail = content[1].split('"')[2]
+        content = line.split("=")
+        variable = content[0].remove("export ")
+        original_value = content[1].split('"')[1]
 
-        line = "export#{variable}=\"#{network_params[variable]}\"#{tail}"
+        if original_value.empty?
+          line = line.insert(line.index('"') + 1, network_params[variable])
+        else
+          line = line.sub original_value, network_params[variable]
+        end
       end
 
       tmp << line
     end
 
     tmp.close
-
     if run_shell_command("cp --no-preserve=mode,ownership #{tmp.path} #{network_variables}")
-      if run_shell_command("alces_RERUN=true bash #{network_setup}")
+      out, err, status = run_global_script(ENV['NETWORK_SET'])
+      if status.success?
         flash[:success] = 'Network configuration successfully modified'
       else
         flash[:danger] = 'Encountered an error whilst trying to run the setup script'
@@ -46,6 +50,11 @@ class NetworkController < ApplicationController
 
   def network_params
     params[:network]
+  end
+
+  def network_get_output
+    out, err, status = run_global_script(ENV['NETWORK_GET'])
+    out.lines.map
   end
 
   def file_data
