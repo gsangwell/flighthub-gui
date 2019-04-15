dc6c7591bbe06b07849e438df0642e3addf8746f
DavidMarchant <davidwjmarchant@gmail.com>
2019-04-15 17:21:07 +0100

    Extract 'get_assets' method

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -7,14 +7,7 @@ class AssetsController < ApplicationController
     if params[:filter_on] and params[:filter_arg]
       cmd = cmd + " --#{params[:filter_on]} #{params[:filter_arg]}"
     end
-    @assets = {}
-    assets_list = execute(cmd)
-    assets_by_type = assets_list.split("\n#")
-    assets_by_type.each do |type_list|
-      next if type_list.empty?
-      type_list = type_list.split("\n")
-      @assets[type_list.shift] = type_list
-    end
+    @assets = get_assets(cmd)
   end
 
   def single_asset
@@ -40,5 +33,18 @@ class AssetsController < ApplicationController
     #See here: https://stackoverflow.com/a/26040994/6257573
     cmd = cmd + ';' unless cmd.match(/;$/)
     Bundler.with_clean_env { @asset_data = Open3.capture3(cmd)[0] }
+  end
+
+  #TODO potentially implement caching to prevent unnecssarily re-executing
+  # this command
+  def get_assets(cmd = 'flight inventory list')
+   assets_by_type = {}
+   asset_type_list = execute(cmd).split("\n#")
+   asset_type_list.each do |type_list|
+     next if type_list.empty?
+     type_list = type_list.split("\n")
+     assets_by_type[type_list.shift] = type_list
+   end
+   assets_by_type
   end
 end
