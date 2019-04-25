efd945237ab3ed8b8c4fdcbc2651b81d00ad8771
Andrew <andrew.toghill@alces-software.com>
2019-04-25 11:31:28 +0100

    Redirect to requested page after logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if User.authenticate(params[:session][:username], params[:session][:password])
       sign_in(user)
-      redirect_to cluster_path
+      redirect_back(fallback_location: cluster_path)
     else
       flash[:danger] = 'Invalid username or password'
       redirect_to login_path
