839b90a1092ea6f3e7c100231dffd49b1e182080
Andrew <andrew.toghill@alces-software.com>
2019-05-17 15:34:53 +0100

    Allow blank input on the asset filter bar

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -4,7 +4,7 @@ class AssetsController < ApplicationController
   def index
     redirect_unless_bolt_on('Assets')
     cmd = "flight inventory list"
-    if params[:filter_on] and !params[:filter_arg].blank?
+    if params[:filter_on]
       cmd = cmd + " --#{params[:filter_on]} #{params[:filter_arg].downcase}"
     end
     @assets = get_assets(cmd)
