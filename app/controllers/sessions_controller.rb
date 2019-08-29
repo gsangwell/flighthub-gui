f0f1a730aaf30f00322d391dcd08ed0ccf2cf530
Andrew <andrew.toghill@alces-software.com>
2019-08-29 15:25:42 +0100

    Authenticate via PAM
    Users are not stored within the DB in the traditional manner. Now when a
    user logs in via a PAM user the username is saved to the DB and nothing
    else.
    
    This requires a PAM user to exist with a password set which is used to
    login on the root page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
   include Clearance::Controller
 
   def create
-    user = User.find_by(username: params[:session][:username])
     if User.authenticate(params[:session][:username], params[:session][:password])
+      user = User.find_or_create_by!(username: params[:session][:username])
       sign_in(user)
       redirect_back(fallback_location: cluster_path)
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
   include Clearance::User
+  require 'rpam2'
 
   def email_optional?
     true
   end
 
+  def password_optional?
+    true
+  end
+
   def self.authenticate(username, password)
-    return nil unless user = find_by_username(username)
-    return user if user.authenticated?(password)
+    Rpam2.auth("system-auth", username, password)
   end
 end
