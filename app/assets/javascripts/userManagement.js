67f83115882baae95b90567bb5cdcbad3baee837
Andrew <andrew.toghill@alces-software.com>
2019-09-10 15:36:41 +0100

    Require password fields as necessary

--- app/assets/javascripts/userManagement.js
+++ app/assets/javascripts/userManagement.js
@@ -3,7 +3,30 @@ function populateModifyModal() {
     var username = e.relatedTarget.dataset.username;
 
     $(e.currentTarget).find('input[name="user_modify[username]"]').val(username)
+
+    document.addEventListener('input', checkPasswordInputs);
   });
 }
 
 document.addEventListener('turbolinks:load', populateModifyModal);
+
+function checkPasswordInputs() {
+  var password_field = document.getElementById('user_modify_password');
+  var confirmation_field = document.getElementById('user_modify_password_confirmation');
+
+  if (password_field.value || confirmation_field.value) {
+    requireField(password_field, true);
+    requireField(confirmation_field, true);
+  } else {
+    requireField(password_field, false);
+    requireField(confirmation_field, false);
+  }
+}
+
+function requireField(field, bool) {
+  if (bool) {
+    field.setAttribute('required', bool);
+  } else {
+    field.removeAttribute('required');
+  }
+}
