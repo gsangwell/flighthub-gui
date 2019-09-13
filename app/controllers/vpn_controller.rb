c7a63b09108a5ffa31c166920f90bb38bd03ab9c
Andrew <andrew.toghill@alces-software.com>
2019-09-13 12:41:22 +0100

    Collate the VPN slots

--- app/controllers/vpn_controller.rb
+++ app/controllers/vpn_controller.rb
@@ -1,4 +1,11 @@
 class VpnController < ApplicationController
+  def index
+    @slots = run_appliance_menu_cmd('vpnStatus')[:output]["vpns"]
+    run_appliance_menu_cmd('vpnSlotsAvail')[:output]["slots"].each do |slot|
+      @slots[slot] = {}
+    end
+  end
+
   def start
     return unless bolt_on_enabled('VPN')
     if run_global_script(ENV['VPN_START'])[:status].success?
