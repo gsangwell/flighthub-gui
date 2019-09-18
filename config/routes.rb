0cbd0db4ad0786d6de0732a9e9e5c9fb53a4228e
Andrew <andrew.toghill@alces-software.com>
2019-09-18 12:11:31 +0100

    Add route to generate a password

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
     get 'vpn', to: 'vpn#index'
     post 'vpn/assign', to: 'vpn#assign'
+    get 'vpn/generate_password/:slot', to: 'vpn#generate_password', as: :vpn_generate_password
 
     get 'console', to: 'console#index'
 
